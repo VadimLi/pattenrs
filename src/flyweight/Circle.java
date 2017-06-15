2b753354baf0f93fb93872ba112c781fbf429c12
Vadim Lipakov <vadimlipakov01@mail.ru>
2017-06-15 19:39:41 +0300

    init commit

---
+++ src/flyweight/Circle.java
@@ -0,0 +1,34 @@
+package flyweight;
+
+public class Circle implements Shape {
+
+    private String color;
+
+    private int x;
+
+    private int y;
+
+    private int radius;
+
+    Circle(final String color) {
+        this.color = color;
+    }
+
+    public void setX(final int x) {
+        this.x = x;
+    }
+
+    public void setY(final int y) {
+        this.y = y;
+    }
+
+    public void setRadius(final int radius) {
+        this.radius = radius;
+    }
+
+    @Override
+    public void draw() {
+        System.out.println("Circle: Draw() [Color : " + color + ", x : " + x + ", y :" + y + ", radius :" + radius);
+    }
+
+}
