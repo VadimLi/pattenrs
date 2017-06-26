dab56aa01b034d121128f55e0eb624797c14877e
Vadim Lipakov <vadimlipakov01@mail.ru>
2017-06-26 21:13:02 +0300

    init commit

---
+++ src/front_controller/HomeView.java
@@ -0,0 +1,10 @@
+package front_controller;
+
+public class HomeView {
+
+    public void show() {
+        System.out.println("Dysplaying Home Page");
+    }
+
+}
+
---
+++ src/front_controller/Main.java
@@ -0,0 +1,11 @@
+package front_controller;
+
+public class Main {
+
+    public static void main(String[] args) {
+        FrontController frontController = new FrontController();
+        frontController.dispatchRequest("HOME");
+        frontController.dispatchRequest("STUDENT");
+    }
+
+}
